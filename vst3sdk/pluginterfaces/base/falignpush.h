93ad081528bfe1a365ee3465c0ab240b72618413
Michael Ost <michael.ost@boseprofessional.com>
2026-03-17 11:07:31 -0700

    suppresses an unneeded pragma pack warning

--- vst3sdk/pluginterfaces/base/falignpush.h
+++ vst3sdk/pluginterfaces/base/falignpush.h
@@ -29,6 +29,8 @@
 	//! @brief warning C4103: alignment changed after including header, may be due to missing #pragma pack(pop)
 	#ifdef _MSC_VER
 		#pragma warning(disable : 4103)
+	#elif defined(__clang__)
+		#pragma GCC diagnostic ignored "-Wpragma-pack"
 	#endif
 	#pragma pack(push)
 	#if SMTG_PLATFORM_64
